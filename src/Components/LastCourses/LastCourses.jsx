f4bff04e808a144de5311657de59185bf6754b11
Hasan <hassanagabalazada81tr@gmail.com>
2024-02-09 16:13:44 +0330

    create ui coursebox

--- src/Components/LastCourses/LastCourses.jsx
+++ src/Components/LastCourses/LastCourses.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import SectionHeader from "../SectionHeader/SectionHeader";
 import "./LastCourses.css";
+import CourseBox from "../CourseBox/CourseBox";
+import "../CourseBox/CourseBox.css";
 export default function LastCourses() {
   return (
     <>
@@ -11,6 +13,21 @@ export default function LastCourses() {
             text={"سکوی پرتاپ شما به سمت موفقیت "}
             btntitle={"تمامی دوره ها"}
           />
+        </div>
+        <div class="courses-content">
+          <div class="container">
+            <div class="row">
+              <CourseBox />
+              <CourseBox />
+              <CourseBox />
+              <CourseBox />
+              <CourseBox />
+              <CourseBox />
+              <CourseBox />
+              <CourseBox />
+              <CourseBox />
+            </div>
+          </div>
         </div>
       </div>
     </>
